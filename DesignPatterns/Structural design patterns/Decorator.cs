6bdec47edab383a6519506eee420a0dd39988184
Liselot Ankone <l.a.ankone@students.uu.nl>
2023-11-05 13:29:01 +0100

    Decorator

---
+++ DesignPatterns/Structural design patterns/Decorator.cs
@@ -0,0 +1,39 @@
+﻿
+public abstract class Component
+{
+        public abstract void Operation();
+}
+
+public class ConcreteComponent : Component
+{
+        public override void Operation()
+        {
+                Console.WriteLine("ConcreteComponent.Operation()");
+        }
+}
+public  abstract class Decorator : Component
+{
+        protected Component component;
+
+        public Decorator(Component component)
+        {
+                this.component = component;
+        }
+
+        public override void Operation(){}
+}
+
+public class ConcreteDecoratorA : Decorator
+{
+        public ConcreteDecoratorA(Component component) : base(component)
+        {
+        }
+
+        public override void Operation()
+        {
+                //voer de originele operation uit
+                component.Operation();
+                //voeg hier nog extra functionaliteit toe
+                Console.WriteLine("You have been decorated :)");
+        }
+}
