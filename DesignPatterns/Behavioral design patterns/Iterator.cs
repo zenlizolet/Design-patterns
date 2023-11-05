cfe189736da84e0b367e6179ef84bf2f5f4a654b
Liselot Ankone <l.a.ankone@students.uu.nl>
2023-11-05 14:32:43 +0100

    iterator is nog niet af

---
+++ DesignPatterns/Behavioral design patterns/Iterator.cs
@@ -0,0 +1,46 @@
+﻿namespace DesignPatterns;
+// work in progress
+// dit is de interface voor de verschillende soorten iterators
+public interface Iterator
+{
+    public int getNext();
+    public bool hasMore();
+}
+/*
+public class ConcreteIterator : Iterator
+{
+    public ConcreteCollection collection;
+    private int iterationState;
+
+//hier nog ff naar kijken
+
+    public ConcreteIterator(ConcreteCollection collection)
+    {
+        this.collection = c;
+        this.iterationState = 0;
+    }
+
+    public void getNext()
+    {
+        this.iterationState++;
+    }
+
+    public bool hasMore()
+    {
+        return this.iterationState < this.aggregate.getSize();
+    }
+}
+
+//dit is de interface die de collectie implementeert
+interface IterableCollection{
+    public Iterator createIterator();
+}
+
+//dit is de concrete collectie
+interface ConcreteCollection : IterableCollection
+{
+    public Iterator CreateIterator()
+    {
+        return new ConcreteIterator(this);
+    }
+}*/
